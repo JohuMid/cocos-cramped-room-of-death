b291d2124531f0621e64e06ac6240df3be4d1f1e
JohuMid <johumid@outlook.com>
2025-06-19 17:28:10 +0800

    feat: 绘制第一关的关卡

--- assets/Scripts/Scene/BattleManager.ts
+++ assets/Scripts/Scene/BattleManager.ts
@@ -1,14 +1,27 @@
 import { _decorator, Component, Node } from 'cc';
+import { TileMapManager } from '../Tile/TileMapManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('BattleManager')
 export class BattleManager extends Component {
+
+    generateTileMap() {
+        const stage = new Node()
+        stage.setParent(this.node)
+        const tileMap = new Node()
+        tileMap.setParent(stage)
+        const tileMapManager = tileMap.addComponent(TileMapManager)
+        tileMapManager.init()
+    }
+
     start() {
+        this.generateTileMap()
 
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
-
+
+
