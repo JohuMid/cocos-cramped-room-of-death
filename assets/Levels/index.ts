cb3fd2eb40438985b75d7164260e266f6df1c741
JohuMid <JohuMid@outlook.com>
2025-06-19 09:02:09 +0800

    feat: 添加关卡数据、枚举类型和关卡管理器。

---
+++ assets/Levels/index.ts
@@ -0,0 +1,22 @@
+import { TILE_TYPE_ENUM } from '../Enums';
+import level1 from './level1';
+
+export interface ITile {
+  src:number | null,
+  type:TILE_TYPE_ENUM | null
+}
+
+export interface ILevel {
+  mapInfo:Array<Array<ITile>>
+}
+
+const levels:Record<string,ILevel> ={
+    level1,
+}
+
+export default levels
+
+/*
+关卡数据
+https://gitee.com/sli97/cocos-cramped-room-of-death/tree/master/assets/Levels
+ */
