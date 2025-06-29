ff6831b211cb1858bc67bc608bd34531dd1c038d
JohuMid <JohuMid@outlook.com>
2025-06-29 13:34:59 +0800

    feat(屏幕震动): 添加方向性屏幕震动效果。

--- assets/Scripts/UI/ShakeManager.ts
+++ assets/Scripts/UI/ShakeManager.ts
@@ -1,6 +1,6 @@
 import { _decorator, Component, game, Node } from 'cc';
 import EventManager from '../../Runtime/EventManager';
-import { CONTROLLER_ENUM, EVENT_ENUM } from '../../Enums';
+import { CONTROLLER_ENUM, EVENT_ENUM, SHAKE_TYPE_ENUM } from '../../Enums';
 const { ccclass, property } = _decorator;
 
 @ccclass('ShakeManager')
@@ -9,6 +9,7 @@ export class ShakeManager extends Component {
 
     private oldTime:number = 0
     private oldPos:{x:number,y:number} = {x:0,y:0}
+    private type:SHAKE_TYPE_ENUM
 
     onLoad(){
       EventManager.Instance.on(EVENT_ENUM.SCREEN_SHAKE,this.onShake,this)
@@ -22,10 +23,12 @@ export class ShakeManager extends Component {
       this.isShaking = false
     }
 
-    onShake(){
+    onShake(type:SHAKE_TYPE_ENUM){
       if (this.isShaking) {
         return
       }
+
+      this.type = type
       this.oldTime = game.totalTime
       this.isShaking = true
       this.oldPos.x = this.node.position.x
@@ -38,14 +41,27 @@ export class ShakeManager extends Component {
 
     update(deltaTime: number) {
       if (this.isShaking) {
-        const duration = 1000
-        const amount = 1.6
+        const duration = 200
+        const amount = 16
         const frequency = 12
         const curSecond = (game.totalTime-this.oldTime)/1000
         const totalSecond = duration/1000
-        const offset = amount * Math.sin(frequency / Math.PI * curSecond)
+        const offset = amount * Math.sin(frequency * Math.PI * curSecond)
 
-        this.node.setPosition(this.oldPos.x+offset,this.oldPos.y+offset)
+        switch (this.type) {
+          case SHAKE_TYPE_ENUM.TOP:
+            this.node.setPosition(this.oldPos.x, this.oldPos.y - offset)
+            break;
+          case SHAKE_TYPE_ENUM.BOTTOM:
+            this.node.setPosition(this.oldPos.x, this.oldPos.y + offset)
+            break;
+          case SHAKE_TYPE_ENUM.LEFT:
+            this.node.setPosition(this.oldPos.x - offset, this.oldPos.y)
+            break;
+          case SHAKE_TYPE_ENUM.RIGHT:
+            this.node.setPosition(this.oldPos.x + offset, this.oldPos.y)
+            break;
+        }
 
         if (curSecond > totalSecond) {
           this.isShaking = false
